800bc20d119598e954321bc56c7510f6baf6fcfb
Hugo Girona <hugo.girona@student.hepl.be>
2025-02-14 09:47:12 +0100

    added new front-page template using Wordpress Hierarchy template

---
+++ wp-content/themes/dw/front-page.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= wp_title('|', false, 'right') . get_bloginfo('name') ?></title>
+</head>
+<body>
+
+<header>
+    <h1><?= get_bloginfo('name') ?></h1>
+    <p><?= get_bloginfo('description') ?></p>
+</header>
+
+<main>
+    <aside>
+        <h2>Bienvenue sur mon site&nbsp;!</h2>
+    </aside>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+
+    <div><?= get_the_content(); ?> </div>
+
+    <?php endwhile; else : ?>
+        <p><?php esc_html_e('La page est vide.'); ?></p>
+    <?php endif; ?>
+</main>
+</body>
+</html>
