58e9f986bd1cb78525205a231322bda7595e1f0f
Hugo Girona <hugo.girona@student.hepl.be>
2025-02-14 10:00:55 +0100

    added header and footer files

--- wp-content/themes/dw/front-page.php
+++ wp-content/themes/dw/front-page.php
@@ -1,18 +1,5 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= wp_title('|', false, 'right') . get_bloginfo('name') ?></title>
-</head>
-<body>
+<?php get_header(); ?>
 
-<header>
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
-</header>
 
 <main>
     <aside>
@@ -26,6 +13,5 @@
     <?php endwhile; else : ?>
         <p><?php esc_html_e('La page est vide.'); ?></p>
     <?php endif; ?>
-</main>
-</body>
-</html>
+
+    <?php get_footer(); ?>
--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,28 +1,12 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= wp_title('|', false, 'right') . get_bloginfo('name') ?></title>
-</head>
-<body>
+<?php get_header(); ?>
 
-<header>
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
-</header>
-
-<main>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <main>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
     <h2><?= get_the_title(); ?></h2>
     <div><?= get_the_content(); ?> </div>
 
-    <?php endwhile; else : ?>
-        <p><?php esc_html_e('La page est vide.'); ?></p>
-    <?php endif; ?>
-</main>
-</body>
-</html>
+<?php endwhile; else : ?>
+    <p><?php esc_html_e('La page est vide.'); ?></p>
+<?php endif; ?>
+<?php get_footer(); ?>
