e632e8d86b419b5a3c1dc384a815522d797b0a2b
Hugo Girona <hugo.girona@student.hepl.be>
2025-02-14 11:55:49 +0100

    added excerpt and thumbnail display and some css

--- wp-content/themes/dw/single-recipe.php
+++ wp-content/themes/dw/single-recipe.php
@@ -23,21 +23,51 @@ Template Name: Page 'Recette'
             width: 320px;
             padding: 20px;
             background-color: lightgray;
+            display: flex;
+            flex-direction: column-reverse;
         }
+
+        .recipe__fig{
+            display: block;
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-top: 100%;
+            margin: 0;
+        }
+
+        .recipe__img{
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
     </style>
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 
         <h2><?= get_the_title(); ?></h2>
+
+        <p><?= get_the_excerpt()?></p>
         <div class="recipe">
             <aside class="recipe__ingredients">
-                <h3>
-                    Ingrédients
-                </h3>
-                <p>
-                    A completer
-                </p>
+                <div>
+                    <h3>
+                        Ingrédients
+                    </h3>
+                    <p>
+                        A completer
+                    </p>
+                </div>
+                <figure class="recipe__fig">
+                    <?= get_the_post_thumbnail( size: 'large', attr: [ 'class' => 'recipe__img' ] ); ?>
+                </figure>
+
             </aside>
 
             <section class="recipe__steps">
